9f76b5abd5860be59891a2a7c324d5ad81c0af40
MLHaddon <haddon.mike2@outlook.com>
2021-03-29 17:04:35 -0500

    update .env

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require("dotenv");
-dotenv.config();
+const port = 8000;
 
 const port = process.env.PORT;
 const app = express();
